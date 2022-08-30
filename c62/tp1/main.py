ef014ff03c957d30d44992b34402aa8fbd263970
Marchand Déric <e.dmarchand@etu.cvm.qc.ca>
2022-08-30 12:25:03 -0400

    bugs on indexes

--- c62/tp1/main.py
+++ c62/tp1/main.py
@@ -4,27 +4,24 @@ import numpy as np
 
 def lecture_texte(fichier):
     return np.array(fichier.read().split())
-    
 
 def main(params):
+    texte_complet = None
+    taille_fenetre = None
+    
     try:
-        fenetre, encodage, chemin = params[1:] # on ignore le premier paramètre qui correspond au nom du fichier
+        taille_fenetre, encodage, chemin = params[1:] # on ignore le premier paramètre qui correspond au nom du fichier
         try:
             fichier = open(chemin, 'r', encoding = f'{encodage}')
-            texte_complet = lecture_texte(fichier)
-            mots_uniques = entrainement.mots_uniques(texte_complet)
-            print(mots_uniques)
-            for mot in enumerate(mots_uniques):
-                print(mot[0])
-            print(entrainement.cooccurrences(mots_uniques))
-            
-            # for ligne in f:
-            #     print(ligne)
+            texte_complet = lecture_texte(fichier)           
             fichier.close()
         except:
             print("Chemin d'accès invalide")
     except:
         print("Veuillez spécifier la taille de la fenêtre, l'encodage et le chemin du fichier")
+        
+    ent = entrainement.Entrainement(texte_complet, int(taille_fenetre))
+
     
 if __name__ == '__main__':
 	quit(main(sys.argv))
